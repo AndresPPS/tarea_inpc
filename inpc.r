5f3b6c57dc7e5970ab007b9a8912164a59af4207
fjbarragans <franciscojbarragans@gmail.com>
2023-02-10 00:09:04 -0600

    pos ahi se fue

--- inpc.r
+++ inpc.r
@@ -1,12 +1,22 @@
+rm(list = ls())
+
 install.packages("shiny")
-
-
+setwd("D:/Progra/Clase5")
 
 library(shiny)
 library(dplyr)
+library(lubridate)
+
+inpc <- read.csv("inpc.csv", header = T, sep = ",", skip = 4)
+inpc <- inpc %>% select(2)
+colnames(inpc) <- "indice"
+
+inpc <- as.ts(inpc, start=c(1970, 1), frequency = 12)
+inpc.arima <- arima(inpc, order = c(2,2,2))
+plot(inpc.arima)
 
 ui <- fluidPage(
-  sliderInput(inputId = "meses", label = "meses (máximo 6)", 
+  sliderInput(inputId = "meses", label = "meses (m?ximo 6)", 
               min = 1, max = 6, value =6),
   
 )
